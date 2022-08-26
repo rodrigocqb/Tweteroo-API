abcec9c43f1a09a90a8bd07a70ed6acc06ddd1c1
rodrigocqb <rodrigocortibarros@gmail.com>
2022-08-26 00:14:40 -0300

    Implemented sign-up

--- index.js
+++ index.js
@@ -2,8 +2,17 @@ import express from "express";
 import cors from "cors";
 
 const app = express();
-app.use(cors());
 
-app.listen(5000, () => {
-    console.log("Listening on 5000");
+const users = [];
+const tweets = [];
+
+app.use(cors());
+app.use(express.json());
+
+app.post("/sign-up", (req, res) => {
+  const user = req.body;
+  users.push(user);
+  res.send("OK");
 });
+
+app.listen(5000, () => console.log("Listening on port 5000"));
